4d43a4e7fd05a7d8938bcda1e8c703a1f7a42216
juan <judagosa8520@gmail.com>
2025-01-20 08:40:54 -0500

    Set up Express server and add initial HTML/CSS files
    - Implemented an Express server in index.js listening on port 4000.
    - Created a public directory with an index.html file containing basic structure.
    - Added styles.css to set the background color of the page to grey.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hola mundo');
+const express = require('express');
+
+// Crear el servidor de express
+const app = express();
+
+
+// Directorio público
+app.use(express.static('public'));
+
+// Rutas
+// app.get('/', (req, res) => {
+//   res.json({
+//     ok: true,
+//     mensaje: 'Hola mundo'
+//   })
+// });
+
+// Escuchar peticiones
+app.listen(4000, () => {
+  console.log(`Servidor corriendo en el puerto 4000`);
+});
