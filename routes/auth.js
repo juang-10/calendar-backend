ecca27f2cf80e69f90ac64b73623f23b791b26a5
juan <judagosa8520@gmail.com>
2025-01-20 20:38:56 -0500

    feat: Add validation middleware and enhance authentication routes
    - Introduced a new middleware `validateFields` to handle request validation using express-validator.
    - Updated the user registration and login routes to include validation checks for name, email, and password.
    - Improved error handling by returning structured error responses when validation fails.

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,32 @@
 // host + /api/auth/
 
 const { Router } = require('express');
-const router = Router();
-
+const { check } = require('express-validator');
+const { validateFields } = require('../middlewares/validate-fields');
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
 
-router.post('/new', createUser);
+const router = Router();
 
-router.post('/', loginUser);
+router.post(
+  '/new', 
+  [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    validateFields,
+  ], 
+  createUser
+);
+
+router.post(
+  '/',
+  [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    validateFields,
+  ],
+  loginUser
+);
 
 router.get('/renew', renewToken);
 
