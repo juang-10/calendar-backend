6945643e0a68f5a6ccb0e7d73abca75b03b83d2f
juan <judagosa8520@gmail.com>
2025-01-21 14:23:03 -0500

    feat: Establish MongoDB connection in server setup
    - Added a new database configuration file (database/config.js) to manage MongoDB connection using mongoose.
    - Integrated the database connection into the main server file (index.js) to ensure the database is connected when the server starts.
    - Improved application structure by separating database logic from server initialization.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('dotenv').config();
-
+const { dbConnection } = require('./database/config');
 // Crear el servidor de express
 const app = express();
 
+// Base de datos
+dbConnection();
 
 // Directorio público
 app.use(express.static('public'));
