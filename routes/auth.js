f98313c2e198a7c01affb8474dc4e3035519b076
juan <judagosa8520@gmail.com>
2025-01-20 20:08:03 -0500

    refactor: Update authentication routes to use controller functions
    - Replaced existing root route with specific routes for user creation, login, and token renewal.
    - Added POST route for user registration and login, and a GET route for token renewal.
    - Improved code organization by utilizing controller functions for handling authentication logic.

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,12 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    ok: true,
-  })
-});
+const { createUser, loginUser, renewToken } = require('../controllers/auth');
+
+router.post('/new', createUser);
+
+router.post('/', loginUser);
+
+router.get('/renew', renewToken);
 
 module.exports = router;
