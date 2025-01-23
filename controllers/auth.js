a4c847e772322888b4a15621afd7c178777dad00
juan <judagosa8520@gmail.com>
2025-01-23 10:30:12 -0500

    feat: Implement password hashing and enhance loginUser function
    - Added bcryptjs for secure password hashing during user creation.
    - Updated createUser function to hash passwords before saving users.
    - Refactored loginUser function to validate user credentials and return appropriate responses.
    - Improved error handling for non-existent users and incorrect passwords.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { response} = require('express');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 const createUser = async(req, res = response) => {
@@ -11,11 +12,16 @@ const createUser = async(req, res = response) => {
     if ( user ) {
       return res.status(400).json({
         ok: false,
-        msg: 'Un usuario ya existe con este correo'
+        msg: 'El usuario ya existe con este email'
       })
     }
 
     user = new User(req.body);
+
+    // Encriptar contraseña
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
     await user.save();
 
     res.status(201).json({
@@ -33,15 +39,42 @@ const createUser = async(req, res = response) => {
   }
 }
 
-const loginUser = (req, res = response) => {
+const loginUser = async(req, res = response) => {
   const { email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: 'Login',
-    email,
-    password,
-  })
+  try {
+    const user = await User.findOne({ email })
+
+    if ( !user ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El usuario no existe con este email'
+      })
+    }
+
+    // Confirmar los passwords
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if ( !validPassword ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Password incorrecto'
+      })
+    }
+
+    // Generar JWT
+
+
+    res.json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+      // token,
+    })
+    
+  } catch (error) {
+    
+  }
 }
 
 const renewToken = (req, res = response) => {
