a6c66fa2656cb9f45ec8269427daf02ba44eafb8
juan <judagosa8520@gmail.com>
2025-01-27 20:39:05 -0500

    refactor: Clean up comments and maintain consistency in event routes

--- routes/events.js
+++ routes/events.js
@@ -1,14 +1,13 @@
 const { Router } = require('express');
 const { validateJWT } = require('../middlewares/validate-jwt');
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
+
 const router = Router();
 
-
 // Todas tienen que pasar por la validación del token
-// Obtener eventos
-
 router.use( validateJWT );
 
+// Obtener eventos
 router.get('/', getEvents);
 
 // Crear un nuevo evento
