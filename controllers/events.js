29307cd7ba727dbe5e0c9eeaffc8f0630c1d5a55
juan <judagosa8520@gmail.com>
2025-02-09 20:18:43 -0500

    feat: Implement updateEvent function with validation and error handling

--- controllers/events.js
+++ controllers/events.js
@@ -35,11 +35,48 @@ const createEvent = async(req, res = response) => {
   }
 }
 
-const updateEvent = (req, res = response) => {
-  res.json({
-    ok: true,
-    event: 'updateEvent',
-  });
+const updateEvent = async (req, res = response) => {
+
+  const eventoID = req.params.id;
+
+  try {
+
+    const event = await Event.findById(eventoID);
+    const uid = req.uid;
+
+    if(!event){
+      return res.status(404).json({
+        ok: false,
+        msg: 'Evento no existe por ese id'
+      });
+    }
+
+    if(event.user.toString() !== uid){
+      return res.status(401).json({
+        ok: false,
+        msg: 'No tiene privilegio de editar este evento'
+      });
+    }
+
+    const newEvent = {
+      ...req.body,
+      user: uid
+    }
+
+    const eventUpdated = await Event.findByIdAndUpdate(eventoID, newEvent, {new: true});
+
+    res.json({
+      ok: true,
+      event: eventUpdated
+    });
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    })
+  }
 }
 
 const deleteEvent = (req, res = response) => {
